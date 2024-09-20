929f89de8e23ee14907825abefbfae8262198686
Cong Minh <ndmc.system176@gmail.com>
2024-09-20 14:16:32 +0700

    Setup Project

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './pages/HomeView.vue'
-import AboutView from './pages/AboutView.vue'
+import LoginPage from './pages/LoginPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    }
-  ]
+  history: createWebHistory(),
+  routes: routes
 })
 
 export default router
