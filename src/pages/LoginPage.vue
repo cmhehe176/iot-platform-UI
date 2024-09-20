5bc9dc9c6245369b56dcc80410adddbeb0105686
Cong Minh <ndmc.system176@gmail.com>
2024-09-20 15:15:29 +0700

    feat: Login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,53 @@
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  username: '',
+  password: ''
+})
+</script>
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
+    <div class="container-login">
+      <div class="preview-login">dd</div>
+      <div class="input-login">
+        <el-form>
+          <el-form-item>
+            <el-input v-model="form.username" placeholder="email or telephone" size="large" />
+          </el-form-item>
+          <el-form-item>
+            <el-input v-model="form.password" placeholder="password" size="large" />
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped type="scss">
+.login {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f0f4f9;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .container-login {
+    width: 50%;
+    height: 50%;
+    background-color: rgb(255, 255, 255);
+    border-radius: 35px;
+
+    .input-login {
+      width: 40%;
+
+      .el-input {
+        height: 50px;
+      }
+    }
+  }
+}
+</style>
